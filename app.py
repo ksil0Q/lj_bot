6bc8d9ce2da9d8fad210397c020ae3fe290a1e2c
ksil0Q <vip.mark7373@mail.ru>
2024-02-09 05:42:53 +0500

    change bot mode to webhooks

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
 import asyncio
 import logging
+import ssl
+from aiogram import Bot
+from aiohttp import web
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
 
+import loader
 from loader import bot, dp
 from middlewares import BlockerMiddleware
 from handlers import common, seller_role, customer_role, car_choosing, detail_menu,\
     creating_advertisement, confirm_payment, user_blocking, closing_advertisement
 
 
-async def startup():
+async def on_startup(bot: Bot) -> None:
+    logging.info("Bot has been started")
+    await bot.set_webhook(f"{loader.WEBHOOK_URL}:{loader.WEB_SERVER_PORT}{loader.WEBHOOK_PATH}")
+    logging.info("Webhook has been set up")
+
+
+async def main():
     logging.basicConfig(level=logging.INFO)
+    logging.getLogger('aiogram').setLevel(logging.DEBUG)
 
     dp.include_routers(customer_role.router, seller_role.router, car_choosing.router,
                        detail_menu.router, creating_advertisement.router, confirm_payment.router,
                        user_blocking.router, closing_advertisement.router,
-                       common.router) # that router always must be the last one
+                       common.router) # that router must always be the last one
     dp.message.outer_middleware(BlockerMiddleware())
-    await dp.start_polling(bot)
+
+    dp.startup.register(on_startup)
+
+    app = web.Application()
+
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot
+    )
+
+    webhook_requests_handler.register(app, path=loader.WEBHOOK_PATH)
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain(loader.WEBHOOK_SSL_CERT, loader.WEBHOOK_SSL_PRIV)
+
+    setup_application(app, dp, bot=bot)
+
+    await web._run_app(app, host=loader.WEB_SERVER_HOST, port=loader.WEB_SERVER_PORT, ssl_context=context)
 
 
 if __name__ == '__main__':
-    asyncio.run(startup())
+    asyncio.run(main())
--- loader.py
+++ loader.py
@@ -8,17 +8,27 @@ load_dotenv()
 
 
 # database
-db = PostgresqlDatabase(database=os.environ.get('DATABASE'),
+db = PostgresqlDatabase(database=os.environ.get('DATABASE_NAME'),
                         user=os.environ.get('USER'),
                         password=os.environ.get('PASSWORD'),
-                        host=os.environ.get('HOST'),
-                        port=os.environ.get('PORT'))
+                        host=os.environ.get('DB_HOST'),
+                        port=os.environ.get('DB_PORT'))
 psql_manager = Manager(database=db, loop=None)
 
 # bot
 bot = Bot(token=os.environ.get('API_TOKEN'), parse_mode='HTML')
 dp = Dispatcher()
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # fix for peewee_async on win
+
+WEB_SERVER_HOST=os.environ.get('WEB_SERVER_HOST')
+WEB_SERVER_PORT=int(os.environ.get('WEB_SERVER_PORT'))
+
+WEBHOOK_PATH=os.environ.get('WEBHOOK_PATH')
+WEBHOOK_URL=os.environ.get('WEBHOOK_URL')
+
+WEBHOOK_SSL_PRIV=os.environ.get("WEBHOOK_SSL_PRIV")
+WEBHOOK_SSL_CERT=os.environ.get("WEBHOOK_SSL_CERT")
+
 
 max_brands_in_row = int(os.environ.get('MAX_BRANDS_IN_ROW'))
 max_models_in_row = int(os.environ.get('MAX_MODELS_IN_ROW'))
